67da9d0b178ddedf50e46d4a9e29445ca1d4c5ab
Rovshan Mammadli <rovshan.mammadli.762@gmail.com>
2024-06-24 19:23:03 +0400

    Init commit

---
+++ django_site/chat/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:room_name>/", views.room, name="room"),
+    path('start_chat/<str:username>',views.start_chat,name="start_chat"),
+    path('video/<str:room_name>/',views.video,name="video"),
+    
+]
